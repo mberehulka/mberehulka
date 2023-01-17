d4dc8e0a733eb83aa89109870e30f88d393a855a
Muriel Guedes <murielguedes@proton.me>
2023-01-17 18:50:47 -0300

    Delete threejs_boilerplate.js

--- threejs_boilerplate.js
+++
@@ -1,7 +0,0 @@
-import * as t from 'three';
-const container = document.getElementById('container');
-const renderer = new t.WebGLRenderer( { antialias: false } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.outputEncoding = t.sRGBEncoding;
-container.appendChild( renderer.domElement );
